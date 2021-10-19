d7a02859b503bac99444a5d1e56359fec0986ad3
AlokSachan <alok.sachan.83@gmail.com>
2021-10-19 20:28:31 +0530

    Initial commit

---
+++ src/test/java/com/alok/jenkins/JenkinsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alok.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
