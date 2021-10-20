1d502f3668187fe5b89e1d4bf5e9e1a4db27398c
AlokSachan <alok.sachan.83@gmail.com>
2021-10-20 21:21:35 +0530

    create new testcase

--- src/test/java/com/alok/jenkins/JenkinsServiceApplicationTests.java
+++ src/test/java/com/alok/jenkins/JenkinsServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package com.alok.jenkins;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+@Slf4j
 class JenkinsServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldShowSimpleAssertion() {
+		log.info("***********************");
+		log.info("inside the testCase");
+		log.info("***********************");
+		Assertions.assertEquals(1, 1);
 	}
 
 }
